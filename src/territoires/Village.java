40cd8104fc0443f20ee624715fa8fdbbf6237fdf
grezit gabriel <grg3890a@ens.fsi>
2022-10-05 15:02:47 +0200

    Save 15:02 TP2

--- src/territoires/Village.java
+++ src/territoires/Village.java
@@ -1,21 +1,49 @@
 package territoires;
 
+import personnages.*;
 public class Village {
 	private String nom;
+	private Chef chef;
+	private Gaulois [] villageois;
+	private int nbVillageois = 0;
 	
 	
-	public Village(String nom) {
+	public Village(String nom, int nbVillageoisMaximum) {
 		this.nom = nom;
-		
+		this.villageois = villageois;
 	}
-
+	
+	public void ajouterHabitant (Gaulois gaulois, int nbVillageoisMaximum) {
+		if (nbVillageois < nbVillageoisMaximum) {
+			villageois[nbVillageois] = gaulois;
+			nbVillageois += 1;
+		} else {
+			System.out.println("Il n'y a plus de place" 
+								+ gaulois.getNom() 
+								+ "est à la recherhche d'un autre village" );
+		}	
+	}
+	public void trouverHabitant(int numVillageois) {
+		System.out.println("Je recherche" 
+							+ villageois[numVillageois]
+							+ "Où est il ?");
+	}
+	
 	public String getNom() {
 		return nom;
 	}
-
+	
+	public void setChef(Chef chef) {
+		 this.chef = chef;
+	 }
 	
 	public static void main(String args[]) {
-		Village alesia = new Village("Alesia");
-		System.out.println(alesia.getNom());
+		Village village = new Village("Village des Irréductible", 30);
+		// Gaulois gaulois = village.trouverHabitant(30); 
+		// 
+		//
+		Chef chef = new chef (Abraracourcix, 6)
+		
+		
 	}
  }
