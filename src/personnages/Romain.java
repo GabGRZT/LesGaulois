8dc731956e6c11837308bfe1aca5ec49f7159747
GabGRZT <114427044+GabGRZT@users.noreply.github.com>
2022-10-15 15:06:00 +0200

    Save les énumérés fin TP2

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -3,11 +3,14 @@ package personnages;
 public class Romain {
 	private String nom;
 	private int force;
+	private Equipement[] equipements;
+	private int nbEquipement = 0;
 	
 	public Romain(String nom, int force) {
 		assert (force>0):"force negative";
 		this.nom = nom;
 		this.force = force;
+		this.equipements = new Equipement[2];
 	}
 	
 	public String getNom() {
@@ -35,6 +38,36 @@ public class Romain {
 		assert (force1 > force):"force romain n'a pas diminue";
 	}
 	
+	private void equipement(Equipement equipement) {
+		equipements[nbEquipement] = equipement;
+		nbEquipement++;
+		System.out.println("Le soldat " + nom + " s'equipe d'un " + 
+				equipement + ".");
+	}
+	
+	public void sEquiper(Equipement equipement) {
+		switch(nbEquipement) {
+				case 2: {
+					System.out.println("Le soldat " + nom + " est deja bien protégé !");
+					break;
+				}
+				case 1: {
+					if (equipements[0] == equipement) {
+						System.out.println("Le soldat " + nom + " possede déjà un " + 
+								equipement + ".");
+						break;
+					} else {
+						equipement(equipement);
+						break;
+					}
+				}
+				default : {
+					equipement(equipement);
+					break;
+				}
+			}
+		}
+	
 	public static void main(String[] args) {
 
 		Romain minus = new Romain ("Minus", 6);
@@ -45,5 +78,9 @@ public class Romain {
 		cesar.recevoirCoup(10);
 		minus.parler("Ne me frappez pas");
 		minus.recevoirCoup(1);
+		minus.sEquiper(Equipement.CASQUE);
+		minus.sEquiper(Equipement.CASQUE);
+		minus.sEquiper(Equipement.BOUCLIER);
+		minus.sEquiper(Equipement.CASQUE);
 	}
 }
